09d62e60ef30abe0e74ba63d2f62e332e9c4d652
aschenbrenner3 <alaschenbrenner@gmail.com>
2017-04-03 16:13:53 -0400

    java hello world todo personal repo

---
+++ hello-world/src/main/java/com/examplehello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.examplehello;
+
+/**
+ * Created by npw383 on 4/3/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
