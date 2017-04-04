2b2f7387a79e526a0e203ecdb0638340b01d119c
aschenbrenner3 <alaschenbrenner@gmail.com>
2017-04-04 13:52:08 -0400

    commit Lab.java Java1

---
+++ java1/src/main/java/com/example/Lab.java
@@ -0,0 +1,124 @@
+package com.example;
+
+public class Lab {
+
+    /*
+     * 1. Cast from double to int
+     *
+     * labels:[primitives, casting]
+     *
+     * f(0.0) = 0
+     * f(3.1) = 3
+     */
+    public static int castToInt(double n) {
+        return (int) n;
+    }
+
+    /*
+     * 2. Cast from short to byte
+     *
+     * labels:[primitives, casting]
+     *
+     * f(2) = 2
+     * f(128) = -128
+     */
+    public static byte castToByte(short n) {
+        return (byte) n;
+    }
+
+    /*
+     * 3. Division
+     *
+     * labels:[operators, exceptions, control statements]
+     *
+     * f(10,2) = 5.0
+     * f(3,2) = 1.5
+     * f(1,0) = throw IllegalArgumentException
+     */
+    public static double divide(double operandOne, double operandTwo) throws IllegalArgumentException {
+
+        if (operandTwo == 0) {
+            throw new IllegalArgumentException("You can't divide by 0, silly!");
+        } else return (operandOne / operandTwo);
+
+    }
+
+    /*
+     * 4. Even
+     *
+     * labels:[operators, control statements]
+     *
+     * f(2) = true
+     * f(3) = false
+     */
+    public static boolean isEven(int n) {
+        if (n % 2 == 0) {
+            return true;
+        } else return false;
+    }
+
+
+    /*
+     * 5. All even
+     *
+     * labels:[operators, arrays, control statements]
+     *
+     * f([2]) = true
+     * f([2,4,6,8,10]) = true
+     *
+     * f([3]) = false
+     * f([2,4,6,8,11]) = false
+     */
+    public static boolean isAllEven(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    /*
+     * 6. Average
+     *
+     * labels:[arrays, operators, control statements, exceptions]
+     *
+     * f([2]) = 2.0
+     * f([2,3]) = 2.5
+     * f(null) = throw IllegalArgumentException
+     */
+    public static double average(int[] array) throws IllegalArgumentException {
+        if (array == null) {
+            throw new IllegalArgumentException("Array is null");
+        }
+
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / ((double) array.length);
+
+    }
+
+    /*
+     * 7. Palindrome
+     *
+     * labels:[strings]
+     *
+     * f("a") = true
+     * f("aba") = true
+     * f("abba") = true
+     *
+     * f("ab") = false
+     */
+    public static boolean isPalindrome(String str) {
+        String str2 = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            str2 += str.charAt(i);
+        }
+        return str.equals(str2);
+    }
+
+}
